6076cb2f05cbb1645562d6472efab3aedc0eab7c
officeshinyujun <officeshinyujun@gmail.com>
2025-08-09 15:03:17 +0900

    fix/flex, hstack, vstack 다른거로 바꿈

--- src/components/hstack/HStack.tsx
+++
@@ -1,7 +0,0 @@
-import React, { Ref } from 'react';
-import { FlexProps, Flex } from '../flex';
-
-export default function HStack(props: FlexProps & { ref?: Ref<any> }) {
-    const { ref, ...rest } = props;
-    return <Flex ref={ref} direction="row" {...rest} />;
-}
