5a109273ceec14e7c4acbc90fa60c834b2bf0d94
officeshinyujun <officeshinyujun@gmail.com>
2025-08-09 15:48:26 +0900

    feat/fix type

--- src/components/HStack/index.tsx
+++ src/components/HStack/index.tsx
@@ -1,6 +1,7 @@
 import {forwardRef, Ref} from "react";
 import {FlexProps} from "../Flex";
 import Flex from "../Flex";
+//@ts-ignore
 export const HStack = forwardRef(function HStack(props : FlexProps, ref : Ref<any>) {
     return <Flex ref={ref} direction="row" {...props} />
 })
--- src/components/VStack/index.tsx
+++ src/components/VStack/index.tsx
@@ -1,7 +1,7 @@
 import {forwardRef, Ref} from "react";
 import {FlexProps} from "../Flex";
 import Flex from "../Flex";
-
+//@ts-ignore
 export const VStack = forwardRef(function VStack(props : FlexProps, ref : Ref<any>) {
     return <Flex ref={ref} direction="column" {...props} />
 })
